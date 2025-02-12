5a4cf8c4575ea1209a2cb39c381ae25620151c33
Madhav Arora <eragon.cube@outlook.com>
2025-02-12 22:34:44 +0530

    Implement logout

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from fastapi import APIRouter, status, Body, HTTPException, Response, Depends
 
-from app.models.user import UserModel, user_collection, UserResponseModel, UserBody, UserPublicModel
+from app.models.user import UserModel, user_collection, UserResponseModel, UserBody
 from app.utils.auth import create_access_token, get_current_user, verify_password
 
 router = APIRouter()
@@ -12,16 +12,13 @@ router = APIRouter()
     "/",
     response_description="Get logged in user",
     status_code=status.HTTP_200_OK,
-    response_model_by_alias=False,
+    response_model_by_alias=True,
     response_model=UserResponseModel,
 )
 async def get_user(current_user=Depends(get_current_user)):
     user = user_collection.find_one({"email": current_user.email})
 
-    return {
-        "message": "You are logged in!",
-        "user": user
-    }
+    return {"message": "You are logged in!", "user": user}
 
 
 @router.post(
@@ -29,7 +26,7 @@ async def get_user(current_user=Depends(get_current_user)):
     response_description="Add new user",
     response_model=UserResponseModel,
     status_code=status.HTTP_201_CREATED,
-    response_model_by_alias=False,
+    response_model_by_alias=True,
 )
 async def create_user(response: Response, user: UserModel = Body(...)):
     """
@@ -38,23 +35,29 @@ async def create_user(response: Response, user: UserModel = Body(...)):
     A unique `id` will be created and provided in the response.
     """
     try:
-        existing_user = user_collection.find_one({"$or": [{"email": user.email}, {"username": user.username}]})
+        existing_user = user_collection.find_one(
+            {"$or": [{"email": user.email}, {"username": user.username}]}
+        )
         if existing_user:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail="User with this email or username already exists")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User with this email or username already exists",
+            )
 
         new_user = user_collection.insert_one(
             user.model_dump(by_alias=True, exclude={"id"})
         )
 
-        created_user = user_collection.find_one(
-            {"_id": new_user.inserted_id}
+        created_user = user_collection.find_one({"_id": new_user.inserted_id})
+
+        access_token = create_access_token(
+            {"sub": user.email}, timedelta(minutes=60 * 24)
         )
 
-        access_token = create_access_token({"sub": user.email}, timedelta(minutes=60 * 24))
-
         # Set cookie
-        response.set_cookie("token", access_token, httponly=True, secure=False, samesite="lax")
+        response.set_cookie(
+            "token", access_token, httponly=True, secure=False, samesite="lax"
+        )
 
         return {
             "message": "User registered successfully",
@@ -62,7 +65,9 @@ async def create_user(response: Response, user: UserModel = Body(...)):
         }
 
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
 
 
 @router.post(
@@ -70,23 +75,44 @@ async def create_user(response: Response, user: UserModel = Body(...)):
     response_description="Login user",
     response_model=UserResponseModel,
     status_code=status.HTTP_200_OK,
-    response_model_by_alias=False,
+    response_model_by_alias=True,
 )
 async def login(response: Response, user: UserBody = Body(...)):
     try:
         identifier = user.identifier
-        db_user = user_collection.find_one({"$or": [{"email": identifier}, {"username": identifier}]})
+        db_user = user_collection.find_one(
+            {"$or": [{"email": identifier}, {"username": identifier}]}
+        )
         if not db_user or not verify_password(user.password, db_user["password"]):
             print(db_user["password"], user.password)
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+            )
 
-        access_token = create_access_token({"sub": db_user["email"]}, timedelta(minutes=60 * 24))
+        access_token = create_access_token(
+            {"sub": db_user["email"]}, timedelta(minutes=60 * 24)
+        )
 
-        response.set_cookie("token", access_token, httponly=True, secure=False, samesite="lax")
+        response.set_cookie(
+            "token", access_token, httponly=True, secure=False, samesite="lax"
+        )
 
         return {
             "message": "User successfully logged in!",
             "user": db_user,
         }
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
+
+@router.post(
+    "/logout",
+    response_description="Logout user",
+    status_code=status.HTTP_200_OK,
+)
+async def logout(response: Response):
+    response.delete_cookie("token")
+    return {"message": "User successfully logged out!"}
